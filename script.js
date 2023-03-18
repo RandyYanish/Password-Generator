d430b8520d0d733d320a5ac1f4e92db1215053d9
RandyYanish <rtaylorfargo@gmail.com>
2023-03-17 20:52:13 -0500

    Password Generator
    Worked on password generator 3-17-23

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code + Event listener when button is pressed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Various Arrays
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var specialCharacter = ["!", "@", "#", "$", "%", "&", "*", "<", ">", "?"]
+
+// Function in #password input
+function generatePassword() {
+ var passwordLength = prompt("How long would you like your password to be?\n(8-128 characters long)");
+
+  // Validate password length
+  while (passwordLength <= 7 || passwordLength >= 129) {
+    alert("Password must be between 8 and 128 characters long!");
+    var passwordLength = prompt("How long would you like your password to be?\n(8-128 characters long)");
+  } 
+
+  // Repeat back the length of the password
+  alert(`Your password will have ${passwordLength} characters!`)
+
+  // Prompts for criteria: lowercase, uppercase, numeric, special characters
+  var lowercaseSelect = confirm("Would you like to include Lowercase letters?")
+  var uppercaseSelect = confirm("Would you like to include Uppercase letters?")
+  var numberSelect = confirm("Would you like to include Numbers?")
+  var specialSelect = confirm("Would you like to include Special characters?")
+
+  // Validate at least one character type selection
+  while (lowercaseSelect === false && uppercaseSelect === false && numberSelect === false && specialSelect === false) {
+    alert ("You must select at least one character type!")
+    var lowercaseSelect = confirm("Would you like to include Lowercase letters?")
+    var uppercaseSelect = confirm("Would you like to include Uppercase letters?")
+    var numberSelect = confirm("Would you like to include Numbers?")
+    var specialSelect = confirm("Would you like to include Special characters?")
+  } 
+  
+  // password generation
+
+}
+
+
+// password displayed  (either in alert or written on the page)
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +49,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
