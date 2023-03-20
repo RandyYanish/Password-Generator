55099c476da4f23e7166af292c833e3c6b6fc75e
RandyYanish <rtaylorfargo@gmail.com>
2023-03-19 19:51:04 -0500

    tied passowrd generation to display

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code + Event listener when button is pressed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Various Arrays
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var specialCharacter = ["!", "@", "#", "$", "%", "&", "*", "<", ">", "?"]
+// Const object to hold various arrays
+const characterSet = {
+  lowerAlphabet: "abcdefghijklmnopqrstuvwxyz",
+  upperAlphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "1234567890",
+  specialCharacter: "`~!@#$%^&*()-_=+\|]}[{;:,<.>/?",
+};
 
 // Function in #password input
 function generatePassword() {
- var passwordLength = prompt("How long would you like your password to be?\n(8-128 characters long)");
+  var passwordCharacters = "";
+
+  var passwordLength = prompt("How long would you like your password to be?\n(8-128 characters long)");
 
   // Validate password length
   while (passwordLength <= 7 || passwordLength >= 129) {
@@ -21,33 +26,39 @@ function generatePassword() {
 
   // Prompts for criteria: lowercase, uppercase, numeric, special characters
   var lowercaseSelect = confirm("Would you like to include Lowercase letters?")
+  if (lowercaseSelect) {passwordCharacters += characterSet.lowerAlphabet}
   var uppercaseSelect = confirm("Would you like to include Uppercase letters?")
+  if (uppercaseSelect) {passwordCharacters += characterSet.upperAlphabet}
   var numberSelect = confirm("Would you like to include Numbers?")
+  if (numberSelect) {passwordCharacters += characterSet.number}
   var specialSelect = confirm("Would you like to include Special characters?")
+  if (specialSelect) {passwordCharacters += characterSet.specialCharacter}
 
   // Validate at least one character type selection
   while (lowercaseSelect === false && uppercaseSelect === false && numberSelect === false && specialSelect === false) {
-    alert ("You must select at least one character type!")
-    var lowercaseSelect = confirm("Would you like to include Lowercase letters?")
-    var uppercaseSelect = confirm("Would you like to include Uppercase letters?")
-    var numberSelect = confirm("Would you like to include Numbers?")
-    var specialSelect = confirm("Would you like to include Special characters?")
-  } 
+    alert ("You must select at least one character type!");
+    var lowercaseSelect = confirm("Would you like to include Lowercase letters?");
+    if (lowercaseSelect) {passwordCharacters += characterSet.lowerAlphabet};
+    var uppercaseSelect = confirm("Would you like to include Uppercase letters?");
+    if (uppercaseSelect) {passwordCharacters += characterSet.upperAlphabet};
+    var numberSelect = confirm("Would you like to include Numbers?");
+    if (numberSelect) {passwordCharacters += characterSet.number};
+    var specialSelect = confirm("Would you like to include Special characters?");
+    if (specialSelect) {passwordCharacters += characterSet.specialCharacter};
+  }; 
+
+  // Repeat back the character type list
+  alert(`Your password will include characters from this list: \n${passwordCharacters}`)
 
   // Empty string to push to
   var randomPassword = "";
 
-  // Filter out unselected character types
-
-
     // password generation
-  for (let i = randomPassword.length; i == passwordLength) {
-    let rand = Math.floor(Math.random() * choice.length)
-  }
+  for (i = 0; i < passwordLength; i++) {
+    randomPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  };
+  return randomPassword;
 }
-
-
-// password displayed  (either in alert or written on the page)
 
 // Write password to the #password input
 function writePassword() {
